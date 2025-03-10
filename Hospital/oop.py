63a5b6d4f5087da8a32880f1e91d25b63a885079
craigkibera <craigituro@gmail.com>
2025-03-10 13:32:04 +0300

    first commit

--- Hospital/oop.py
+++
@@ -1,9 +0,0 @@
-a = [1, 2, 3, 4, 5, 6, 7]
-
-for integer in a:
-
-    #condition to check if even numbers
-    if integer % 2 == 0:
-        print("This is an even number")
-    else:
-            print ("odd number") 
